86ca421d442064326c9dcfa1a1b2a255547c9276
Byter <vitazagrebelnyh@gmail.com>
2024-10-22 22:07:17 +1000

    add url with products

--- Karmen-dress-store/urls.py
+++ Karmen-dress-store/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('products/', include('products.urls', namespace='products')),
+    path('', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:
