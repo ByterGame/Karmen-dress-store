b61661b32da66fd53ed20e7b754b53efe61e7a46
yobysheva <bobysheva.yv@students.dvfu.ru>
2024-10-23 20:14:05 +1000

    templates integrated

--- Karmen-dress-store/settings.py
+++ Karmen-dress-store/settings.py
@@ -1,22 +1,14 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-7$&ue%r@0och!@gbjq@xu2ofx&k32(1ju2w2%1=b(%r_ex@mh!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -59,20 +51,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Karmen-dress-store.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -89,9 +73,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+]
 
 LANGUAGE_CODE = 'en-us'
 
@@ -101,16 +85,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- products/views.py
+++ products/views.py
@@ -1,10 +1,23 @@
 from products.models import Dress, DressCategory
 from django.shortcuts import render
 
-def products(request):
+def index(request):
     context = {
         'title': 'Karmen Dress',
         'products': Dress.objects.all(),
         'categories': DressCategory.objects.all(),
     }
-    return render(request, 'products/products.html', context)
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+def single_product(request):
+    return render(request, 'single-product.html')
+
