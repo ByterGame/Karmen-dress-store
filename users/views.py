16453e38d5bf433dfcfad9c521ddf82d4b01b5dc
yobysheva <bobysheva.yv@students.dvfu.ru>
2024-10-29 20:44:19 +1000

    model windows errors showing

--- users/views.py
+++ users/views.py
@@ -7,7 +7,6 @@ from django.http import JsonResponse
 
 def sign_in(request):
     form = UserLoginForm(data=request.POST or None)
-    referer_url = request.META.get('HTTP_REFERER', 'products:index')
     if request.method == 'POST':
         if form.is_valid():
             username = form.cleaned_data['username']
@@ -15,41 +14,69 @@ def sign_in(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
-                #HttpResponseRedirect(referer_url)
                 return JsonResponse({'status': 'success', 'message': 'You successfully logged in.'})
-            # else:
-            #     form = UserLoginForm()
-            #     context = {'form': form}
-            #     messages.error(request, 'Invalid login or password.')
-            #     return render(request, referer_url, context)
-            #     #return JsonResponse({'status': 'error', 'message': 'Invalid login or password.'})
+            else:
+                return JsonResponse({'status': 'error', 'errors': {'__all__': ['Invalid login or password.']}})
         else:
-            form = UserLoginForm()
-            context = {'form': form}
-            #messages.error(request, 'Invalid login or password.')
-            #return render(request, referer_url, context)
-            return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
-    return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
-
+            return JsonResponse({'status': 'error', 'errors': form.errors})
+    return JsonResponse({'status': 'error', 'errors': {'__all__': ['Invalid request method.']}})
 
 def sign_up(request):
     form = UserRegisterForm(data=request.POST or None)
-    referer_url = request.META.get('HTTP_REFERER', 'products:index')
     if request.method == 'POST':
         if form.is_valid():
             form.save()
-            # form = UserLoginForm()
-            # context = {'form': form}
-            #messages.success(request, 'Your account has been created!')
-            #return HttpResponseRedirect(referer_url)
-            # return render(request, referer_url, context)
             return JsonResponse({'status': 'success', 'message': 'Account created successfully!'})
-    else:
-        # form = UserLoginForm()
-        # context = {'form': form}
-        #return render(request, referer_url, context)
-        return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
-    return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
+        else:
+            return JsonResponse({'status': 'error', 'errors': form.errors})
+    return JsonResponse({'status': 'error', 'errors': {'__all__': ['Invalid request method.']}})
+
+#
+# def sign_in(request):
+#     form = UserLoginForm(data=request.POST or None)
+#     referer_url = request.META.get('HTTP_REFERER', 'products:index')
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = auth.authenticate(username=username, password=password)
+#             if user:
+#                 auth.login(request, user)
+#                 #HttpResponseRedirect(referer_url)
+#                 return JsonResponse({'status': 'success', 'message': 'You successfully logged in.'})
+#             # else:
+#             #     form = UserLoginForm()
+#             #     context = {'form': form}
+#             #     messages.error(request, 'Invalid login or password.')
+#             #     return render(request, referer_url, context)
+#             #     #return JsonResponse({'status': 'error', 'message': 'Invalid login or password.'})
+#         else:
+#             form = UserLoginForm()
+#             context = {'form': form}
+#             #messages.error(request, 'Invalid login or password.')
+#             #return render(request, referer_url, context)
+#             return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
+#     return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
+#
+
+# def sign_up(request):
+#     form = UserRegisterForm(data=request.POST or None)
+#     referer_url = request.META.get('HTTP_REFERER', 'products:index')
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             form.save()
+#             # form = UserLoginForm()
+#             # context = {'form': form}
+#             #messages.success(request, 'Your account has been created!')
+#             #return HttpResponseRedirect(referer_url)
+#             # return render(request, referer_url, context)
+#             return JsonResponse({'status': 'success', 'message': 'Account created successfully!'})
+#     else:
+#         # form = UserLoginForm()
+#         # context = {'form': form}
+#         #return render(request, referer_url, context)
+#         return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
+#     return JsonResponse({'status': 'error', 'message': 'Inwalid login or password.'})
 
 def logout(request):
     auth.logout(request)
